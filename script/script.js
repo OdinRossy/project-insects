1b22266ecede408310d7a35315c77663f1f5a7b9
Gleb Remniov <OdinRossy@users.noreply.github.com>
2018-05-26 20:34:17 +0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".first").click(function () {
+        alert($(".first").text())
+    });
+    $(".second").click(function () {
+        alert($(".second").text())
+    });
+    $(".third").click(function () {
+        alert($(".third").text())
+    });
+});
