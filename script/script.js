a5b1f377bf417206f3f9ccc070db3bec56f7a802
Gleb Remniov <OdinRossy@users.noreply.github.com>
2018-05-31 20:19:44 +0300

    Initial commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
-function scrolling() {
-
-}
+$(function () {
+    let numberOfCorrect = 0;
+    $('#button').click(function () {
+        if ($('#q1').prop('checked') === true && $('#q2').prop('checked') === false && $('#q3').prop('checked') === false && $('#q4').prop('checked') === false) {
+            numberOfCorrect++;
+            // window.alert("Верно " + numberOfCorrect);
+        }
+        if ($('#q5').prop('checked') === true && $('#q6').prop('checked') === false && $('#q7').prop('checked') === false && $('#q8').prop('checked') === false) {
+            numberOfCorrect++;
+            // window.alert("Верно " + numberOfCorrect);
+        }
+        if ($('#q9').prop('checked') === true && $('#q10').prop('checked') === false && $('#q11').prop('checked') === false && $('#q12').prop('checked') === false) {
+            numberOfCorrect++;
+            // window.alert("Верно " + numberOfCorrect);
+        }
+        if ($('#q13').prop('checked') === true && $('#q14').prop('checked') === false && $('#q15').prop('checked') === false && $('#q16').prop('checked') === false) {
+            numberOfCorrect++;
+            // window.alert("Верно " + numberOfCorrect);
+        }
+        alert("Вы ответили правильно на " + numberOfCorrect + " вопросов.")
+    });
+});
